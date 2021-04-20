fdfe2e7035720df847e3e8b2c6769bc28f181411
user <kamauri84@gmail.com>
2021-04-20 15:09:24 +1000

    update icons

--- src/components/ProjectLayout.js
+++ src/components/ProjectLayout.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import {Container,Col,Row,Tab, Tabs,Nav} from 'react-bootstrap'
-import * as Icons from 'react-icons/bs';
-import * as BiIcons from 'react-icons/bi';
-import * as GoIcon from "react-icons/go";
+import {Container,Col,Row,Tab,Nav} from 'react-bootstrap'
+import * as Icon from '@material-ui/icons';
 import Overview from '../containers/Pages/Overview'
+import Indicators from '../containers/Pages/Indicators';
+import Team from '../containers/Pages/Team';
+
 
 
 
@@ -13,7 +14,7 @@ const ProjectLayout = () => {
         <Container>
             <Row className="bg-light">
                 <Col className="col-lg-2 col-12 d-flex justify-content-center">
-                   <Icons.BsPeopleCircle size="130" className="p-1"/>  
+                   <Icon.People size="130" className="p-1"/>  
                 </Col>
                 <Col className="col-lg-7 col-12 d-flex align-items-center justify-content-lg-start justify-content-center">
                     <p>Project Name</p>
@@ -21,52 +22,58 @@ const ProjectLayout = () => {
                 <Col className="col-lg-3 col-12 d-flex justify-content-end flex-column ">
                     <Row className="">
                         <Col className=" col-12 d-flex justify-content-end">
-                        <BiIcons.BiShareAlt size="20"/>
+                        <Icon.Share size="20"/>
 
                         </Col>
                     </Row>
                     <Row>
                         <Col className=" col-12 d-flex align-items-end justify-content-end mt-3">
-                        <Icons.BsPeopleCircle className="mr-2 mb-3"/>
-                        <Icons.BsPeopleCircle className="mr-2 mb-3"/>
-                        <Icons.BsPeopleCircle className="mr-2 mb-3"/>
-                        <Icons.BsPeopleCircle className="mr-2 mb-3"/>
-                        <Icons.BsPeopleCircle className="mr-2 mb-3"/>
-                        <GoIcon.GoPlus className="mr-2 mb-3"/>
+                        <Icon.People className="mr-2 mb-3"/>
+                        <Icon.People className="mr-2 mb-3"/>
+                        <Icon.People className="mr-2 mb-3"/>
+                        <Icon.People className="mr-2 mb-3"/>
+                        <Icon.People className="mr-2 mb-3"/>
+                        <Icon.Add className="mr-2 mb-3"/>
                         </Col>
                      
                     </Row>
                 </Col>
             </Row>
                
-                    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                    <Tab.Container id="left-tabs" defaultActiveKey="Overview">
                         <Row className="mt-4">
-                            <Col lg={3} sm={12} className="">
-                            <Nav variant="pills" className="sticky-top flex-column">
+                            <Col lg={3} sm={12} >
+                            <Nav variant="pills" className="sticky-top flex-column pt-4">
                                 <Nav.Item>
-                                <Nav.Link eventKey="first" className="d-flex justify-content-center">Overview</Nav.Link>
+                                <Nav.Link eventKey="Overview" className="d-flex justify-content-center">Overview</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                <Nav.Link eventKey="second" className="d-flex justify-content-center disabled">Insights</Nav.Link>
+                                <Nav.Link eventKey="Insights" className="d-flex justify-content-center disabled">Insights</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                <Nav.Link eventKey="third" className="d-flex justify-content-center">Indicators</Nav.Link>
+                                <Nav.Link eventKey="Indicators" className="d-flex justify-content-center">Indicators</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                <Nav.Link eventKey="fourth" className="d-flex justify-content-center disabled">Dashboards</Nav.Link>
+                                <Nav.Link eventKey="Dashboards" className="d-flex justify-content-center disabled">Dashboards</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                <Nav.Link eventKey="fifth" className="d-flex justify-content-center disabled">Dashsets</Nav.Link>
+                                <Nav.Link eventKey="Datasets" className="d-flex justify-content-center disabled">Datasets</Nav.Link>
                                 </Nav.Item>
                                 <Nav.Item>
-                                <Nav.Link eventKey="sixth" className="d-flex justify-content-center">Team</Nav.Link>
+                                <Nav.Link eventKey="Team" className="d-flex justify-content-center">Team</Nav.Link>
                                 </Nav.Item>
                             </Nav>
                             </Col>
                             <Col sm={9}>
                             <Tab.Content>
-                                <Tab.Pane eventKey="first">
+                                <Tab.Pane eventKey="Overview">
                                 <Overview />
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="Indicators">
+                                <Indicators />
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="Team">
+                                <Team/>
                                 </Tab.Pane>
                             </Tab.Content>
                             </Col>
